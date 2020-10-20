43bd3f686b03ed86cf6b090f7e3a26a15ed9c392
Dominik Swiatek <dominik.swiatek1995@gmail.com>
2020-10-20 17:48:04 +0200

    update css

--- src/components/main/Navibar/Navbar.js
+++ src/components/main/Navibar/Navbar.js
@@ -24,6 +24,15 @@ const Navbar = () => {
                     </NavLink>
                 </li>
 
+
+                <li className="nav_item">
+                    <NavLink to="/calendar">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            height="35" viewBox="0 0 24 24" width="35" fill="#969292">
+                            <path d="M0 0h24v24H0z" fill="none" /><path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z" /></svg>
+                        <span className="link_text">{words.Calendar}</span>
+                    </NavLink>
+                </li>
                 <li className="nav_item">
                     <NavLink to="/workouts">
                         <svg xmlns="http://www.w3.org/2000/svg"
@@ -32,14 +41,6 @@ const Navbar = () => {
                         <span className="link_text">{words.Workout}</span>
                     </NavLink>
 
-                </li>
-                <li className="nav_item">
-                    <NavLink to="/calendar">
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            height="35" viewBox="0 0 24 24" width="35" fill="#969292">
-                            <path d="M0 0h24v24H0z" fill="none" /><path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z" /></svg>
-                        <span className="link_text">{words.Calendar}</span>
-                    </NavLink>
                 </li>
                 <li className="nav_item">
                     <NavLink to="/previous">
